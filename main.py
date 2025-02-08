eec454b1ee2491631abfbdeceb12377d30f51598
Yura <157220715+YuraKozliakov@users.noreply.github.com>
2025-02-08 22:19:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from password_generator.gui import PasswordGeneratorGUI
+
+def main():
+    root = tk.Tk()
+    app = PasswordGeneratorGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
